96013a8c2f012f65ee454d71755d77d768f04385
JD Flynn <jflynn8@gmail.com>
2018-06-25 20:45:38 -0500

    cors.

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ const port = process.env.PORT || 4001;
 const index = require("./routes/index");
 const app = express();
 
-app.use(index);
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+app.use(index);
 
 const server = http.createServer(app);
 const io = socketIo(server); // < Interesting!
