ef76b983dcbbe0f474bcb9d94e26b542a01d7601
JD Flynn <jflynn8@gmail.com>
2018-06-26 16:05:24 -0500

    let's try again

--- server.js
+++ server.js
@@ -1,42 +1,23 @@
 const express = require("express");
 const http = require("http");
+const https = require("https");
 const socketIo = require("socket.io");
 const axios = require("axios");
 const cors = require("cors");
+const fs = require("fs");
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 4001;
 const sslPort = 4001;
 const index = require("./routes/index");
 const app = express();
 
+const server = https.createServer(app);
+const io = socketIo(server); // < Interesting!
 
 app.use(cors());
 app.use(index);
 
-app.use(function (req, res, next) {
-console.log(req);
-    // Website you wish to allow to connect
-    res.header('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', false);
-
-    // Pass to next layer of middleware
-    next();
-});
-app.use(index);
-
-const server = http.createServer(app);
-const io = socketIo(server); // < Interesting!
-
-io.origins("*.*");
+io.origins("*:*");
 
 io.on("connection", socket => {
   let room;
